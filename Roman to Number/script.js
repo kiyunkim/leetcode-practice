a1a4514d1148423d513d178e68950700b3d4e313
Kiyun <kiyunkim@users.noreply.github.com>
2021-05-06 13:00:59 -0400

    roman to number start

---
+++ Roman to Number/script.js
@@ -0,0 +1,33 @@
+// Convert Roman numerals to integers
+
+// 1    I
+// 5    V
+// 10   X
+// 50   L
+// 100  C
+// 500  D
+// 1000 M
+
+// largest: MMMCMXCIX 3,999
+// Test: III, IV, IX, LVIII, MCMXCIV
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  var num = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+  }
+
+   
+
+};
+
+//
